4d4666eaaaecc1d139b8dbbc4708549bf7d9c3c8
Cnmirpuri1981 <cnmirpuri1981@gmail.com>
2020-07-01 15:03:47 +0700

    Initial Commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,8 @@
+namespace Core.Controllers.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Core.Controllers.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Controllers.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
